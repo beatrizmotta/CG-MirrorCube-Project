fd4e0cdc97218ce68a7b55493bc84061719e78e3
Beatriz Motta <bigobaldohoran@gmail.com>
2023-09-28 19:13:43 -0300

    First commit

--- Assets/RotateCube.cs
+++ Assets/RotateCube.cs
@@ -4,94 +4,94 @@ using UnityEngine;
 
 public class RotateCube : MonoBehaviour
 {
-    private Vector3 rotationAxis = Vector3.up;
-    private float rotationAngle = 90.0f;
-    private float rotationSpeed = 90.0f;
-    private Vector3 pivotPoint;
+    // private Vector3 rotationAxis = Vector3.up;
+    // private float rotationAngle = 90.0f;
+    // private float rotationSpeed = 90.0f;
+    // private Vector3 pivotPoint;
 
-    public int axisIndex = 0; 
-
-
-    private bool isRotating = false;
-    private Quaternion startRotation;
-    private Quaternion endRotation;
-
-    private List<RotatePieces> frontFaces = new List<RotatePieces>();
-    private List<RotatePieces> backFaces = new List<RotatePieces>();
-    private List<RotatePieces> topFaces = new List<RotatePieces>();
-    private List<RotatePieces> bottomFaces = new List<RotatePieces>();
-    private List<RotatePieces> leftFaces = new List<RotatePieces>();
-    private List<RotatePieces> rightFaces = new List<RotatePieces>();
-
-    void Start()
-    {
-        RotatePieces[] pieces = FindObjectsByType<RotatePieces>(FindObjectsSortMode.None);
-        MountFaces(pieces);
-    }
-
-    void Update()
-    {
-        Rotate();
-    }
+    // public int axisIndex = 0; 
 
 
-    public void Rotate()
-    {
-        if (Input.GetKeyDown(KeyCode.R) && !isRotating)
-        {
+    // private bool isRotating = false;
+    // private Quaternion startRotation;
+    // private Quaternion endRotation;
 
-            Vector3 pivot = new Vector3(-0.416999996f,0,-0.653999984f);
+    // private List<RotatePieces> frontFaces = new List<RotatePieces>();
+    // private List<RotatePieces> backFaces = new List<RotatePieces>();
+    // private List<RotatePieces> topFaces = new List<RotatePieces>();
+    // private List<RotatePieces> bottomFaces = new List<RotatePieces>();
+    // private List<RotatePieces> leftFaces = new List<RotatePieces>();
+    // private List<RotatePieces> rightFaces = new List<RotatePieces>();
 
-            endRotation = Quaternion.AngleAxis(rotationAngle, rotationAxis) * transform.rotation;
-            pivotPoint = transform.TransformPoint(pivot);
-            startRotation = transform.rotation;
-            isRotating = true;
+    // void Start()
+    // {
+    //     RotatePieces[] pieces = FindObjectsByType<RotatePieces>(FindObjectsSortMode.None);
+    //     MountFaces(pieces);
+    // }
 
-            axisIndex = axisIndex == 0 ? 1 : 0; 
-        }
+    // void Update()
+    // {
+    //     Rotate();
+    // }
 
-        if (isRotating)
-        {
-            float step = rotationSpeed * Time.deltaTime;
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, endRotation, step);
 
-            if (Quaternion.Angle(transform.rotation, endRotation) < 0.01f)
-            {
-                transform.rotation = endRotation;
-                isRotating = false;
-            }
+    // public void Rotate()
+    // {
+    //     if (Input.GetKeyDown(KeyCode.R) && !isRotating)
+    //     {
 
-        }
-    }
+    //         Vector3 pivot = new Vector3(-0.416999996f,0,-0.653999984f);
 
-    public void MountFaces(RotatePieces[] pieces)
-    {
-        foreach (var piece in pieces)
-        {
-            if (piece.isFront)
-            {
-                frontFaces.Add(piece);
-            }
-            if (piece.isBack)
-            {
-                backFaces.Add(piece);
-            }
-            if (piece.isTop)
-            {
-                topFaces.Add(piece);
-            }
-            if (piece.isBottom)
-            {
-                bottomFaces.Add(piece);
-            }
-            if (piece.isLeft)
-            {
-                leftFaces.Add(piece);
-            }
-            if (piece.isRight)
-            {
-                rightFaces.Add(piece);
-            }
-        }
-    }
+    //         endRotation = Quaternion.AngleAxis(rotationAngle, rotationAxis) * transform.rotation;
+    //         pivotPoint = transform.TransformPoint(pivot);
+    //         startRotation = transform.rotation;
+    //         isRotating = true;
+
+    //         axisIndex = axisIndex == 0 ? 1 : 0; 
+    //     }
+
+    //     if (isRotating)
+    //     {
+    //         float step = rotationSpeed * Time.deltaTime;
+    //         transform.rotation = Quaternion.RotateTowards(transform.rotation, endRotation, step);
+
+    //         if (Quaternion.Angle(transform.rotation, endRotation) < 0.01f)
+    //         {
+    //             transform.rotation = endRotation;
+    //             isRotating = false;
+    //         }
+
+    //     }
+    // }
+
+    // public void MountFaces(RotatePieces[] pieces)
+    // {
+    //     foreach (var piece in pieces)
+    //     {
+    //         if (piece.isFront)
+    //         {
+    //             frontFaces.Add(piece);
+    //         }
+    //         if (piece.isBack)
+    //         {
+    //             backFaces.Add(piece);
+    //         }
+    //         if (piece.isTop)
+    //         {
+    //             topFaces.Add(piece);
+    //         }
+    //         if (piece.isBottom)
+    //         {
+    //             bottomFaces.Add(piece);
+    //         }
+    //         if (piece.isLeft)
+    //         {
+    //             leftFaces.Add(piece);
+    //         }
+    //         if (piece.isRight)
+    //         {
+    //             rightFaces.Add(piece);
+    //         }
+    //     }
+    // }
 }
--- Assets/RotatePieces.cs
+++ Assets/RotatePieces.cs
@@ -5,96 +5,96 @@ using UnityEngine;
 
 public class RotatePieces : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // // Start is called before the first frame update
 
-    Transform myFace;
-    string currentFace; 
-    public bool isFront, isBack, isRight, isLeft, isTop, isBottom;
+    // Transform myFace;
+    // string currentFace; 
+    // public bool isFront, isBack, isRight, isLeft, isTop, isBottom;
 
-    private bool isRotating, hasAligned;
+    // private bool isRotating, hasAligned;
 
-    RotateCube cube;
+    // RotateCube cube;
 
-    public Vector3[] axisOfRotation = {Vector3.forward, Vector3.right};
-
-
-    void Start()
-    {
-        myFace = GameObject.Find("Front").transform;
-        cube = FindObjectOfType<RotateCube>();
-    }
-
-    // Update is called once per frame
+    // public Vector3[] axisOfRotation = {Vector3.forward, Vector3.right};
 
 
-    void Update()
-    {
+    // void Start()
+    // {
+    //     myFace = GameObject.Find("Front").transform;
+    //     cube = FindObjectOfType<RotateCube>();
+    // }
 
-        // bool Physics.Raycast(Ray ray, out RaycastHit hitInfo, float maxDistance, int layerMask)
+    // // Update is called once per frame
+
+
+    // void Update()
+    // {
+
+    //     // bool Physics.Raycast(Ray ray, out RaycastHit hitInfo, float maxDistance, int layerMask)
         
 
 
-        if (Input.GetMouseButtonDown(0))
-        {   
-            print(cube.axisIndex);
-            isRotating = true;
-        }
-        else if (Input.GetMouseButtonUp(0))
-        {
-            isRotating = false;
-            hasAligned = false;
-        }
+    //     if (Input.GetMouseButtonDown(0))
+    //     {   
+    //         print(cube.axisIndex);
+    //         isRotating = true;
+    //     }
+    //     else if (Input.GetMouseButtonUp(0))
+    //     {
+    //         isRotating = false;
+    //         hasAligned = false;
+    //     }
 
-        Vector3 pivot;
-        if (isRotating)
-        {
-            float xRotation = Input.GetAxis("Mouse X");
-            pivot = myFace.eulerAngles;
+    //     Vector3 pivot;
+    //     if (isRotating)
+    //     {
+    //         float xRotation = Input.GetAxis("Mouse X");
+    //         pivot = new Vector3(1.44157875f,-0.735907078f,0.183637679f);
 
-            // pivot = new Vector3(0, 0, 0);
+    //         // pivot = new Vector3(0, 0, 0);
 
-            if (isFront) {
-                transform.RotateAround(pivot, Vector3.forward, xRotation * -1 * 5f);
-            }
+    //         if (isFront) {
+    //             transform.RotateAround(pivot, Vector3.forward, xRotation * -1 * 5f);
+    //         }
 
 
 
-        } else if (!hasAligned) {
-            pivot = myFace.eulerAngles;
-             if (isFront) {
+    //     } else if (!hasAligned) {
+    //         pivot = new Vector3(1.44157875f,-0.735907078f,0.183637679f);
+    //          if (isFront) {
             
-            float currentZRotation = transform.eulerAngles.z;
-            float alignedRotation = NearestStraightMultiple(currentZRotation);
+    //         float currentZRotation = transform.eulerAngles.z;
+    //         float alignedRotation = NearestStraightMultiple(currentZRotation);
 
-            transform.RotateAround(pivot, Vector3.forward, alignedRotation);
+    //         transform.RotateAround(pivot, Vector3.forward, alignedRotation);
 
-            hasAligned = true; 
-        }
-        }
-    }
-
-
+    //         hasAligned = true; 
+    //     }
+    //     }
+    // }
 
 
 
-    public static float NearestStraightMultiple(float angle) {
 
-        float normalizedAngle; 
 
-        if (angle < 0) { 
-            normalizedAngle = -angle % -90f; 
-        } else {
-            normalizedAngle = angle % 90f; 
-        }
+    // public static float NearestStraightMultiple(float angle) {
 
-        float distanceToPrevious = Mathf.Abs(normalizedAngle - 0);
-        float distanceToNext = Mathf.Abs(normalizedAngle - 90);
+    //     float normalizedAngle; 
 
-        if (distanceToNext < distanceToPrevious) {
-            return distanceToNext;
-        } else {
-            return -distanceToPrevious;
-        }
+    //     if (angle < 0) { 
+    //         normalizedAngle = -angle % -90f; 
+    //     } else {
+    //         normalizedAngle = angle % 90f; 
+    //     }
 
-    }
+    //     float distanceToPrevious = Mathf.Abs(normalizedAngle - 0);
+    //     float distanceToNext = Mathf.Abs(normalizedAngle - 90);
+
+    //     if (distanceToNext < distanceToPrevious) {
+    //         return distanceToNext;
+    //     } else {
+    //         return -distanceToPrevious;
+    //     }
+
+    // }
 }
