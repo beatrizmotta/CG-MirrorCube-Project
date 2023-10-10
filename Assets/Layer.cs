2d8605b0a0541bd57922d337f2467c87a2d3ffb8
Beatriz Motta <bigobaldohoran@gmail.com>
2023-10-10 09:34:51 -0300

    feat: added rotation logic (not complete)

--- Assets/Layer.cs
+++ Assets/Layer.cs
@@ -3,16 +3,74 @@ using System.Collections;
 using UnityEngine;
 using App;
 
-public class Layer: MonoBehaviour {
+public class Layer : MonoBehaviour
+{
     public LayerIdentifier identifier;
     public List<Piece> pieces;
+    public Vector3 pivotPoint;
+    private bool movementComplete;
+    private Collider layerCollider;
 
-    public void OnTriggerExit(Collider collision) {}
-    
-    public void OnTriggerEnter(Collider collision) {
-        if (collision.gameObject.tag == "Piece") {
+    private void Start()
+    {
+        // Get the Collider component on startup
+        layerCollider = GetComponent<Collider>();
+    }
+
+
+    public void OnTriggerExit(Collider collision) { }
+
+    public void OnTriggerEnter(Collider collision)
+    {
+        if (collision.CompareTag("Piece") && !movementComplete)
+        {
             Piece piece = collision.gameObject.GetComponent<Piece>();
             this.pieces.Add(piece);
         }
+
+
     }
+
+    public void CompleteMovement()
+    {
+        movementComplete = true;
+    }
+
+    // Call this method when a new movement starts
+    public void StartMovement()
+    {
+        movementComplete = false;
+    }
+
+    // Detect pieces in contact with the layer using Physics.OverlapBox
+    public List<Piece> DetectPiecesInContact()
+    {
+        // Ensure the layer collider is not a trigger
+        layerCollider.isTrigger = false;
+
+        List<Piece> piecesInContact = new List<Piece>();
+
+        // Calculate the center based on the collider's bounds
+        Vector3 layerCenter = layerCollider.bounds.center;
+
+        // Use Physics.OverlapBox to detect pieces in contact
+        Collider[] colliders = Physics.OverlapBox(layerCenter, layerCollider.bounds.size, Quaternion.identity);
+
+        foreach (Collider collider in colliders)
+        {
+            if (collider.CompareTag("Piece"))
+            {
+                Piece piece = collider.gameObject.GetComponent<Piece>();
+                piecesInContact.Add(piece);
+            }
+        }
+
+        // Restore the trigger status
+        layerCollider.isTrigger = true;
+
+        return piecesInContact;
+    }
+
+
+
 }
--- Assets/LayerManager.cs
+++ Assets/LayerManager.cs
@@ -1,34 +1,84 @@
 using UnityEngine;
 using System;
 using App;
+using System.Collections.Generic;
 
-public class LayerManager: MonoBehaviour {
-    public void Update() {
-        if (Input.GetKeyDown(KeyCode.Return)) {
+public class LayerManager : MonoBehaviour
+{
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
             this.RotateLayer(LayerMovement.TopClockwise);
         }
     }
-    
-    public void RotateLayer(LayerMovement movement) {
-        Layer layer = this.GetLayerByMovement(movement);
-        Debug.Log(layer.pieces);
 
-        switch(movement) {
-            case LayerMovement.TopClockwise: {
-                layer.transform.Rotate(Vector3.up, 90f, Space.Self);
+    public void RotateLayer(LayerMovement movement)
+    {
+        Layer layer = this.GetLayerByMovement(movement);
+
+        // Start the movement by setting movementComplete to false
+        layer.StartMovement();
+
+        switch (movement)
+        {
+            case LayerMovement.FrontClockwise:
+            {
+                foreach (Piece piece in layer.pieces)
+                {
+                    piece.transform.RotateAround(layer.pivotPoint, Vector3.forward, 90);
+                    // piece.transform.Rotate(Vector3.up, 90f, Space.Self);
+                }
+
+                // After movement, mark it as complete
+                layer.CompleteMovement();
+
+                // Detect pieces in contact with the layer and update the pieces list
+                List<Piece> piecesInContact = layer.DetectPiecesInContact();
+                foreach(Piece piece in piecesInContact) {
+                    print(piece);
+                }
+                layer.pieces.Clear();
+                layer.pieces.AddRange(piecesInContact);
+
+                break;
+            }
+            case LayerMovement.RightClockwise:
+            {
+                foreach (Piece piece in layer.pieces)
+                {
+                    piece.transform.RotateAround(layer.pivotPoint, Vector3.right, 90);
+                    // piece.transform.Rotate(Vector3.up, 90f, Space.Self);
+                }
+
+                // After movement, mark it as complete
+                layer.CompleteMovement();
+
+                // Detect pieces in contact with the layer and update the pieces list
+                List<Piece> piecesInContact = layer.DetectPiecesInContact();
+                foreach(Piece piece in piecesInContact) {
+                    print(piece);
+                }
+                layer.pieces.Clear();
+                layer.pieces.AddRange(piecesInContact);
+
                 break;
             }
         }
     }
 
-    private Layer GetLayerByMovement(LayerMovement movement) {
+    private Layer GetLayerByMovement(LayerMovement movement)
+    {
         LayerIdentifier? identifier = LayerManager.GetLayerIdentifier(movement);
         GameObject[] objects = GameObject.FindGameObjectsWithTag("Layer");
 
-        if (Enum.IsDefined(typeof(LayerIdentifier), identifier)) {
-            foreach (GameObject item in objects) {
+        if (Enum.IsDefined(typeof(LayerIdentifier), identifier))
+        {
+            foreach (GameObject item in objects)
+            {
                 Layer layer = item.GetComponent<Layer>();
-                if (layer.identifier == identifier) {
+                if (layer.identifier == identifier)
+                {
                     return layer;
                 }
             }
@@ -38,15 +88,17 @@ public class LayerManager: MonoBehaviour {
         throw new ArgumentException(message, nameof(movement));
     }
 
-    private static LayerIdentifier GetLayerIdentifier(LayerMovement movement) {
-        switch(movement) {
+    private static LayerIdentifier GetLayerIdentifier(LayerMovement movement)
+    {
+        switch (movement)
+        {
             case LayerMovement.TopClockwise or LayerMovement.TopAntiClockwise: return LayerIdentifier.Top;
             case LayerMovement.BackClockwise or LayerMovement.BackAntiClockwise: return LayerIdentifier.Back;
             case LayerMovement.LeftClockwise or LayerMovement.LeftAntiClockwise: return LayerIdentifier.Left;
             case LayerMovement.RightClockwise or LayerMovement.RightAntiClockwise: return LayerIdentifier.Right;
             case LayerMovement.FrontClockwise or LayerMovement.FrontAntiClockwise: return LayerIdentifier.Front;
             case LayerMovement.BottomClockwise or LayerMovement.BottomAntiClockwise: return LayerIdentifier.Bottom;
-            default: return (LayerIdentifier) (-1);
+            default: return (LayerIdentifier)(-1);
         }
     }
 }
